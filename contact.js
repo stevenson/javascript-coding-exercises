12ee4d7e852e01fbec0b6c196acfef96c568b4c9
stevenson <stevenson.cunanan@gmail.com>
2022-04-30 01:03:46 +0800

    added fix for bug in one of the scenarios

--- contact.js
+++ contact.js
@@ -4,22 +4,20 @@ function contacts(queries) {
     let responses = [];
     for(let index=0; index < queries.length; index++){
         let query = queries[index];
-        console.log(query);
         if(query[0]==='add'){
-            for(let i=1; i< query[1].length; i++){
+            for(let i=1; i<= query[1].length; i++){
                 let key = query[1].substring(0,i);
                 if(directory[key]){
                     directory[key] = directory[key] + 1;
                 }else{
                     directory[key] = 1;
                 }
-                console.log(directory);
             }
         }else{
-            responses.push(directory[query[1]]|| 0);
+            let count = directory[query[1]] || 0;
+            responses.push(count);
         }
     }
-    
     return responses;
 }
 
