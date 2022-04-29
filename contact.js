a2b4d7645642fc61b94c7f3b2c75c0975527a0ca
stevenson <stevenson.cunanan@gmail.com>
2022-04-29 23:41:29 +0800

    added some javascript solutions for some hackerrank challenges

---
+++ contact.js
@@ -0,0 +1,46 @@
+function contacts(queries) {
+    // Write your code here
+    let directory = {};
+    let responses = [];
+    for(let index=0; index < queries.length; index++){
+        let query = queries[index];
+        console.log(query);
+        if(query[0]==='add'){
+            for(let i=1; i< query[1].length; i++){
+                let key = query[1].substring(0,i);
+                if(directory[key]){
+                    directory[key] = directory[key] + 1;
+                }else{
+                    directory[key] = 1;
+                }
+                console.log(directory);
+            }
+        }else{
+            responses.push(directory[query[1]]|| 0);
+        }
+    }
+    
+    return responses;
+}
+
+function find(directory, query){
+    for(key in directory){
+        if(key === query){
+            return directory[key];
+        }
+    }
+    return 0; 
+}
+
+console.log(contacts([
+    ['add', 's'],
+    ['add', 'ss'],
+    ['add', 'sss'],
+    ['add', 'ssss'],
+    ['add', 'sssss'],
+    ['find', 's'],
+    ['find', 'ss'],
+    ['find', 'sss'],
+    ['find', 'ssss'],
+    ['find', 'sssss'],
+]));
